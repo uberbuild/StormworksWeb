5aca1517e6f494bd53485d8b021ca9b214e7b651
Aaron Davis <>
2023-10-12 19:23:32 +0100

    Add serial port output

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿using System.Net;
-
+﻿using System.IO.Ports;
+using System.Net;
 using System.Text;
 
 namespace StormworksWeb;
@@ -7,18 +7,24 @@ namespace StormworksWeb;
 public class Program
 {
     private static int _numberOfRequests = 0;
-    private static string  _speedKmph = "0";
-    private static string  _speedMph = "0";
-    private static string  _bearing = "0";
-    private static string  _altitude = "0";
-    
+    private static string _speedKmph = "0";
+    private static string _speedMph = "0";
+    private static string _bearing = "0";
+    private static string _altitude = "0";
+    private static SerialPort _serialPort = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+
     public static void Main()
     {
+        _serialPort.PortName = SetPortName("COM1");
+        _serialPort.ReadTimeout = 500;
+        _serialPort.WriteTimeout = 500;
+        _serialPort.Open();
+
         var httpListener = new HttpListener();
         httpListener.Prefixes.Add(("http://localhost:8000/"));
         httpListener.Prefixes.Add(("http://127.0.0.1:8000/"));
         httpListener.Start();
-        
+
         Console.WriteLine("Listening on port: 8000");
 
         while (true)
@@ -33,12 +39,16 @@ public class Program
         var request = context.Request;
         var response = context.Response;
         _numberOfRequests++;
-        
+
         _speedKmph = request.QueryString.Get("kmph") ?? _speedKmph;
         _speedMph = request.QueryString.Get("mph") ?? _speedMph;
         _bearing = request.QueryString.Get("bearing") ?? _bearing;
         _altitude = request.QueryString.Get("alt") ?? _altitude;
-        
+
+        var serialText = $"{_speedKmph}:{_speedMph}:{_altitude}:{_bearing}";
+        _serialPort.WriteLine(serialText);
+        Console.WriteLine(serialText);
+
         var responseString = $"<html>" +
                              $"<body>" +
                              $"<h1>Received {request.HttpMethod} request from {request.UserHostAddress}.</h1>" +
@@ -56,5 +66,25 @@ public class Program
         var responseOutput = response.OutputStream;
         responseOutput.Write(buffer, 0, buffer.Length);
         responseOutput.Close();
+    }
+
+    // Display Port values and prompt user to enter a port.
+    private static string SetPortName(string defaultPortName)
+    {
+        Console.WriteLine("Available Ports:");
+        foreach (string s in SerialPort.GetPortNames())
+        {
+            Console.WriteLine("   {0}", s);
+        }
+
+        Console.Write("Enter COM port value (Default: {0}): ", defaultPortName);
+        string portName = Console.ReadLine() ?? "";
+
+        if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+        {
+            portName = defaultPortName;
+        }
+
+        return portName;
     }
 }
---
+++ arduino.cpp
@@ -0,0 +1,64 @@
+﻿#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+float kmh = 0;
+float alt = 0;
+float mph = 0;
+String serialData = "";
+float bng = 0;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+byte shape[8] = {0b11100,0b10100,0b11100,0b00000,0b00000,0b00000,0b00000,0b00000};
+
+//setup variables here, kmh, mph, bng and alt
+void setup() {
+    lcd.begin(16, 2);
+    Serial.begin(9600);
+    lcd.createChar(0, shape);
+    Serial.setTimeout(100);
+}
+
+void loop() {
+    if (Serial.available()) {
+        serialData = Serial.readString(); // Read the incoming data as a string
+        splitData(serialData);
+    } else {
+        delay(100);
+    }
+    String kmhst = String(kmh, 1) + "kmh";
+    String altst = String(alt, 1) + "l";
+    String mphst = String(mph, 1) + "mph";
+    String bngst = String(bng, 0);
+
+    int spacesforvar1 = 16 - kmhst.length() - altst.length();
+    for (int i = 0; i< spacesforvar1; i++) {
+        kmhst += " ";
+    }
+    int spacesforvar2 = 16 - mphst.length() - bngst.length() - 1;
+    for (int i = 0; i< spacesforvar2; i++) {
+        mphst += " ";
+    }
+    String line1 = kmhst + altst;
+    String line2 = mphst + bngst ;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(line1);
+    lcd.setCursor(0,1);
+    lcd.print(line2);
+    lcd.setCursor(15,1);
+    lcd.write(byte(0));
+
+}
+void splitData(String data) {
+    int separator1 = data.indexOf(':');
+    int separator2 = data.indexOf(':', separator1 + 1);
+    int separator3 = data.indexOf(':', separator2 + 1);
+
+    if (separator1 != -1 && separator2 != -1 && separator3 != -1) {
+        kmh = data.substring(0, separator1).toFloat();
+        mph = data.substring(separator1 + 1, separator2).toFloat();
+        alt = data.substring(separator2 + 1, separator3).toFloat();
+        bng = data.substring(separator3 + 1).toFloat();
+    }
+}
